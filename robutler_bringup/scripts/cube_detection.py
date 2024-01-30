04fd428637bbb2e1ca33ecc155a7bbd07541bf63
lapa <diogo.lapa@ua.pt>
2024-01-30 11:44:02 +0000

    sphere and cube counter

--- robutler_bringup/scripts/cube_detection.py
+++ robutler_bringup/scripts/cube_detection.py
@@ -11,37 +11,36 @@ class CubeDetector:
     def __init__(self):
         rospy.init_node('sphere_detector', anonymous=True)
         self.bridge = CvBridge()
-        self.image_acquired = False
         self.count_pub = rospy.Publisher('/cubes/count', Int32, queue_size=10)
         self.image_sub = rospy.Subscriber('/camera2/rgb/image_raw', Image, self.callback)
         rospy.loginfo('Subscribing to cameras topic')
         rospy.sleep(1)
 
     def callback(self, data):
-        if not self.image_acquired:
-            try:
-                cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
-            except CvBridgeError as e:
-                rospy.logerr(e)
-                return
+            
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+        except CvBridgeError as e:
+            rospy.logerr(e)
+            return
 
-            mask = cv2.inRange(cv_image, (0,230,0), (20,255,20))
+        mask = cv2.inRange(cv_image, (0,230,0), (20,255,20))
 
-            contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-            count = 0
+        count = 0
 
-            for contour in contours:
-                area = cv2.contourArea(contour)
-                if area > 200:
-                    count += 1
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if area > 200:
+                count += 1
 
-            self.count_pub.publish(count)
-            self.image_acquired = True
+        self.count_pub.publish(count)
 
 def main():
     
     cube_detector = CubeDetector()
+    rospy.spin()
 
 if __name__ == '__main__':
     
--- robutler_bringup/scripts/sphere_detection.py
+++ robutler_bringup/scripts/sphere_detection.py
@@ -11,43 +11,41 @@ class SphereDetector:
     def __init__(self):
         rospy.init_node('sphere_detector', anonymous=True)
         self.bridge = CvBridge()
-        self.image_acquired = False
         self.count_pub = rospy.Publisher('/spheres/count', Int32, queue_size=10)
         self.image_sub = rospy.Subscriber('/camera2/rgb/image_raw', Image, self.callback)
         rospy.loginfo('Subscribing to cameras topic')
         rospy.sleep(1)
 
     def callback(self, data):
-        if not self.image_acquired:
-            try:
-                cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
-            except CvBridgeError as e:
-                rospy.logerr(e)
-                return
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+        except CvBridgeError as e:
+            rospy.logerr(e)
+            return
 
-            mask = cv2.inRange(cv_image, (90,0,90), (255,20,255))
+        mask = cv2.inRange(cv_image, (90,0,90), (255,20,255))
 
-            contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-            count = 0
+        count = 0
 
-            for contour in contours:
-                area = cv2.contourArea(contour)
-                if area > 300:
-                    perimeter = cv2.arcLength(contour, True)
-                    circularity = 4 * np.pi * (area / (perimeter * perimeter))
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if area > 200:
+                perimeter = cv2.arcLength(contour, True)
+                circularity = 4 * np.pi * (area / (perimeter * perimeter))
 
-                    circularity_threshold = 0.8
+                circularity_threshold = 0.8
 
-                    if circularity > circularity_threshold:
-                        count += 1
+                if circularity > circularity_threshold:
+                    count += 1
 
-            self.count_pub.publish(count)
-            self.image_acquired = True
+        self.count_pub.publish(count)
 
 def main():
     
     sphere_detector = SphereDetector()
+    rospy.spin()
 
 if __name__ == '__main__':
     
